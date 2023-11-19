96afe7a34e97ea90d52e7ea1d2a5f27a449b3b78
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-19 13:28:29 +0400

    calculator changes commit

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Probability Calculator by Calculator.iO
+Plugin Name: Probability Calculator by www.calculator.io
 Plugin URI: https://www.calculator.io/probability-calculator/
 Description: The probability calculator can find two events' probability and the normal distribution probability. Learn more about probability's laws and calculations.
 Version: 1.0.0
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Probability Ca
 
 function display_ci_probability_calculator(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/probability-calculator/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Probability Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_probability_calculator_iframe"></iframe></div>';
+    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Probability Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_probability_calculator_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_probability_calculator', 'display_ci_probability_calculator' );
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ The probability calculator can find two events' probability and the normal distr
 
 ## Installation
 
-1. [Download](https://github.com/pub-calculator-io/age-calculator/archive/refs/heads/master.zip) the ZIP file of this repository.
+1. [Download](https://github.com/pub-calculator-io/probability-calculator/archive/refs/heads/master.zip) the ZIP file of this repository.
 2. Upload the /probability-calculator-master/ folder to the /wp-content/plugins/ directory.
 3. Activate the [Probability Calculator](https://www.calculator.io/probability-calculator/ "Probability Calculator Homepage") plugin through the "Plugins" menu in WordPress.
 
